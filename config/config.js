7dbae6d304846670d34b45bd05db2fcdbc2fa245
GOSEI75 <ismael.gonzalezseco@gmail.com>
2020-06-20 13:21:58 +0200

    Sección 12 terminada

--- config/config.js
+++ config/config.js
@@ -1,3 +1,6 @@
-
-
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '711136514398-i2va4beiphjsug6722cdvdhlmpsp6qv0.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '6Kh989CpbNQL_y5rxz38CORs';
